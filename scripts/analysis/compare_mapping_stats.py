718c6773caba6aec0892224a525c74f7f8dac3f1
pbilling <pbilling@stanford.edu>
2016-03-21 18:43:53 -0700

    Fixed bug where uniquely-mapped reads overwrote mapped reads

--- scripts/analysis/compare_mapping_stats.py
+++ scripts/analysis/compare_mapping_stats.py
@@ -12,7 +12,7 @@ import shutil
 import fnmatch
 import datetime
 
-def parse_dx_stats_json(json_file):
+def parse_stats_json(json_file):
     with open(json_file, 'r') as JSON:
         data = json.load(JSON)
  
@@ -43,8 +43,11 @@ def parse_dx_stats_json(json_file):
         for category in sample['Read 2']['Unique']:
             unique_pf_reads_2 += sample['Read 2']['Unique'][category]
         insert_size_list.append(int(sample['Mean Insert Size']))
-    mean_insert_size = numpy.mean(insert_size_list)
-       
+    try:
+        mean_insert_size = int(numpy.mean(insert_size_list))
+    except:
+        mean_insert_size = 'NA'
+
     lane_metrics = {
                     'post_filter_reads': post_filter_reads,
                     'mapped_pf_reads_1': mapped_pf_reads_1,
@@ -55,19 +58,19 @@ def parse_dx_stats_json(json_file):
                    }
     return lane_metrics
   
-def parse_sample_stats_csv(csv_path):
+def parse_stats_csv(csv_path):
     lane_metrics = {}
     with open(csv_path, 'r') as CSV:
         for line in CSV:
-            post_filter_match = re.search(r'Post-Filter Reads,"([\d,]+)"', line)
-            mapped_pf_reads_1_match = re.search(r'Mapped PF Reads \(Read 1\),\"([\d,]+)\"', line)
-            mapped_pf_reads_2_match = re.search(r'Mapped PF Reads \(Read 2\),\"([\d,]+)\"', line)
-            unique_pf_reads_1_match = re.search(r'Uniquely-Mapped PF Reads \(Read 1\),\"([\d,]+)\"', line)
-            unique_pf_reads_2_match = re.search(r'Uniquely-Mapped PF Reads \(Read 2\),\"([\d,]+)\"', line)
-            insert_size_match = re.search(r'Insert Size,([\d,]+)', line)
+            post_filter_match = re.search(r'^Post-Filter Reads,"([\d,]+)"', line)
+            mapped_pf_reads_1_match = re.search(r'^Mapped PF Reads \(Read 1\),\"([\d,]+)\"', line)
+            mapped_pf_reads_2_match = re.search(r'^Mapped PF Reads \(Read 2\),\"([\d,]+)\"', line)
+            unique_pf_reads_1_match = re.search(r'^Uniquely-Mapped PF Reads \(Read 1\),\"([\d,]+)\"', line)
+            unique_pf_reads_2_match = re.search(r'^Uniquely-Mapped PF Reads \(Read 2\),\"([\d,]+)\"', line)
+            insert_size_match = re.search(r'^Insert Size,([\d,]+)', line)
 
             if post_filter_match:
-                lane_metrics['post_filter_reads'] = int(post_filter_match.group(1).replace(',',''))
+                lane_metrics['post_filter_reads'] = int(post_filter_match.group(1).replace(',','')) * 2
             elif mapped_pf_reads_1_match:
                 lane_metrics['mapped_pf_reads_1'] = int(mapped_pf_reads_1_match.group(1).replace(',',''))
             elif mapped_pf_reads_2_match:
@@ -78,7 +81,6 @@ def parse_sample_stats_csv(csv_path):
                 lane_metrics['unique_pf_reads_2'] = int(unique_pf_reads_2_match.group(1).replace(',',''))
             elif insert_size_match:
                 lane_metrics['mean_insert_size'] = int(insert_size_match.group(1).replace(',',''))
-    print lane_metrics
     return lane_metrics
 
 def get_dx_stats_jsons(output_dir):
@@ -120,7 +122,7 @@ def parse_dx_stats(stats_json_dir, lane_metrics):
             lane_name = elements[0]
             json_path = os.path.join(stats_json_dir, file)
             print 'Info: Getting dx mapping stats for %s' % file
-            lane_metrics['dx'][lane_name] = parse_dx_stats_json(json_file=json_path)
+            lane_metrics['dx'][lane_name] = parse_stats_json(json_file=json_path)
 
 def parse_scg_stats(stats_csv_dir, lane_metrics):
     for file in os.listdir(stats_csv_dir):
@@ -128,34 +130,16 @@ def parse_scg_stats(stats_csv_dir, lane_metrics):
         if match:
             elements = file.split('.')
             lane_name = elements[0]
-            lane_path = os.path.join(scg_csv_dir, file)
+            lane_path = os.path.join(stats_csv_dir, file)
             print 'Info: Getting mapping stats for %s' % file
-            lane_metrics['scg'][lane_name] = parse_scg_stats_csv(csv_path=lane_path)
+            lane_metrics['scg'][lane_name] = parse_stats_csv(csv_path=lane_path)
 
-def main():
+def write_output(output_dir, lane_metrics):
 
-    dx_mapping_stats_dir = '/srv/gsfs0/projects/gbsc/workspace/pbilling/dx_mapping_stats'
-    scg_mapping_stats_dir = '/srv/gsfs0/projects/gbsc/workspace/pbilling/scg_mapping_stats'
-    analysis_dir = '/srv/gsfs0/projects/gbsc/workspace/pbilling/seq_stats_analysis'
-    scg_2016_pub_dir = '/srv/gsfs0/projects/gbsc/SeqCenter/Illumina/PublishedResults/2016'
-
-    # Get files
-    #get_dx_stats_jsons(output_dir = dx_mapping_stats_dir)
-    #get_scg_stats_csvs(scg_pub_dir = scg_2016_pub_dir)
-
-    # For DX files, I think I will need to use json module to do parsing/addition
-    lane_metrics = {
-                    'dx': {},
-                    'scg': {}
-                   }
-
-    parse_scg_stats(scg_mapping_stats_dir, lane_metrics)
-    parse_dx_stats(dx_mapping_stats_dir, lane_metrics)
-
-    # Write metrics to outfile
     timestamp = str(datetime.datetime.now()).split()[0] # yyyy-mm-dd
-    out_file = '%s_mapping_stats.txt' % 
-    with open(out_file, 'w') as OUT:
+    out_file = '%s_mapping_stats.txt' % timestamp
+    out_path = os.path.join(output_dir, out_file)
+    with open(out_path, 'w') as OUT:
         dx_header = 'dx_post_filter_reads\tdx_mapped_pf_1\tdx_mapped_pf_2\tdx_unique_pf_1\tdx_unique_pf_2\tdx_insert_size\t'
         scg_header = 'scg_post_filter_reads\tscg_mapped_pf_1\tscg_mapped_pf_2\tscg_unique_pf_1\tscg_unique_pf_2\tscg_insert_size\n'
         header = 'lane_name\t' + dx_header + scg_header
@@ -213,20 +197,45 @@ def main():
                 out_str += '%s\t' % str(lane_metrics['scg'][lane]['unique_pf_reads_1'])
             except:
                 out_str += 'NA\t'
-                print 'Warning: Could not get unique_pf_reads_1 value for dx:%s' % lane
+                print 'Warning: Could not get unique_pf_reads_1 value for scg:%s' % lane
             try:
                 out_str += '%d\t' % int(lane_metrics['scg'][lane]['unique_pf_reads_2'])
             except:
                 out_str += 'NA\t'
                 print 'Warning: Could not get unique_pf_reads_2 value for scg:%s' % lane
             try:
-                out_str += '%s\t' % str(lane_metrics['scg'][lane]['mean_insert_size'])
+                out_str += '%s\n' % str(lane_metrics['scg'][lane]['mean_insert_size'])
             except:
-                out_str += 'NA\t'
+                out_str += 'NA\n'
                 print 'Warning: Could not get mean_insert_size value for scg:%s' % lane
             OUT.write(out_str)
 
-    pdb.set_trace()
+
+def main():
+
+    dx_mapping_stats_dir = '/srv/gsfs0/projects/gbsc/workspace/pbilling/dx_mapping_stats'
+    scg_mapping_stats_dir = '/srv/gsfs0/projects/gbsc/workspace/pbilling/scg_mapping_stats'
+    analysis_dir = '/srv/gsfs0/projects/gbsc/workspace/pbilling/seq_stats_analysis'
+    scg_2016_pub_dir = '/srv/gsfs0/projects/gbsc/SeqCenter/Illumina/PublishedResults/2016'
+
+    # Test
+    #scg_mapping_stats_dir = '/srv/gsfs0/projects/gbsc/workspace/pbilling/scg_test'
+    # Get files
+    #get_dx_stats_jsons(output_dir = dx_mapping_stats_dir)
+    #get_scg_stats_csvs(scg_pub_dir = scg_2016_pub_dir)
+
+    # For DX files, I think I will need to use json module to do parsing/addition
+    lane_metrics = {
+                    'dx': {},
+                    'scg': {}
+                   }
+
+    parse_scg_stats(scg_mapping_stats_dir, lane_metrics)
+    parse_dx_stats(dx_mapping_stats_dir, lane_metrics)
+
+    write_output(output_dir=analysis_dir, lane_metrics=lane_metrics)
     
+    #pdb.set_trace()
+
 if __name__ == '__main__':
     main()
