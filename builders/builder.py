ed4a73b7e0ffc670118c5eb8dba1afb09b0c5011
Paul Billing-Ross <pbilling@stanford.edu>
2016-06-16 16:10:13 -0700

    Workflow config templates are now static files. Individual workflow JSONs stored in launchpad

--- builders/builder.py
+++ builders/builder.py
@@ -39,7 +39,9 @@ class WorkflowBuild:
         self.project_key = self.environment['project_key']
         self.project_dxid = self.environment['project_dxid']
         self.branch = self.environment['git_branch']
+        self.commit = self.environment['git_commit']
         self.current_version = self.environment['version']
+        self.dx_OS = self.environment['dx_OS']
 
         # Logic for choosing applet path in DXProject; used by Applet:write_config_file()
         if self.project_key == 'develop':
@@ -49,46 +51,59 @@ class WorkflowBuild:
                                                 self.workflow_name
                                                 )
         else:
-            self.workflow_dxpath = os.path.join('/', self.current_version, self.workflow_name)
+            self.workflow_dxpath = os.path.join('/', 
+                                                self.current_version, 
+                                                self.workflow_name
+                                                )
         self.workflow_logger.info('Workflow path on DNAnexus will be: %s:%s' % (self.project_dxid, self.workflow_dxpath))
 
-        # Load workflow configuration object
+        # Create workflow configuration object
         workflow_config = WorkflowConfig(path_list = path_list, 
                                          project_dxid = self.project_dxid,
-                                         name = self.workflow_name
+                                         name = self.workflow_name,
+                                         dx_OS = self.dx_OS
                                          )
 
         # Build all applets listed in workflow
         for applet_name in workflow_config.applets:
             self.workflow_logger.info('Building %s applet' % applet_name)
+            # Initiate applet assembly on launchpad
             applet = Applet(name = applet_name, 
                             version = self.current_version, 
                             path_list = path_list,
                             logger = self.applet_logger
                             )
             self.workflow_logger.info('Applet initialized')
+            # Add applet internal resources
             internal_rscs = workflow_config.applets[applet_name]['internal_rscs']
             internal_rsc_manager.add_applet_internal_rscs(applet=applet, 
                                                           internal_rscs=internal_rscs, 
                                                           )
+            # Add applet external resources
             external_rscs = workflow_config.applets[applet_name]['external_rscs']
             external_rsc_manager.add_applet_external_rscs(applet=applet, 
                                                           external_rscs=external_rscs
                                                           )
+            # Build applet on DNAnexus
             applet.write_config_file(project_dxid = self.project_dxid)
             applet_id = applet.build(project_dxid = self.project_dxid,
-                                     folder_path = self.workflow_dxpath)
+                                     folder_path = self.workflow_dxpath
+                                     )
             workflow_config.applets[applet_name]['dxid'] = applet_id['id']
             self.workflow_logger.info('Build complete: %s applet id: %s' % (applet_name, applet_id))
         
         # Create workflow 
-        workflow_properties = {
-                               'name': self.workflow_name,
-                               'branch': self.branch, 
-                               'version': self.current_version,
-                               'date_created': str(datetime.datetime.now()).split()[0] # yyyy-mm-dd
-                              }
-        workflow_config.create_workflow(path=self.workflow_dxpath, properties=workflow_properties)
+        workflow_details = {
+                            'name': self.workflow_name,
+                            'branch': self.branch, 
+                            'version': self.current_version,
+                            'commit': self.commit,
+                            'date_created': str(datetime.datetime.now()).split()[0] # yyyy-mm-dd
+                           }
+        # Create DXWorkflow object on DNAnexus
+        workflow_config.create_workflow(path = self.workflow_dxpath, 
+                                        details = workflow_details
+                                        )
         for stage_index in range(0, len(workflow_config.stages)):
             self.workflow_logger.info('Setting executable for stage %d' % stage_index)
             workflow_config.set_stage_executable(str(stage_index))
@@ -97,7 +112,7 @@ class WorkflowBuild:
             self.workflow_logger.info('Setting inputs for stage %d' % stage_index)
             workflow_config.set_stage_inputs(str(stage_index))
         
-        workflow_config.update_config_file(path_list)
+        workflow_config.write_workflow_json(path_list, self.current_version)
         self.workflow_logger.info('Build complete: %s workflow id: {%s, %s}' % (self.workflow_name, 
                                                                        workflow_config.project_dxid,
                                                                        workflow_config.object_dxid
@@ -142,44 +157,47 @@ class WorkflowBuild:
                        'external_rscs_dxid': build_config['external_rscs_project']['dxid'],
                        'git_branch': git_branch,
                        'git_commit': git_commit,
-                       'version': version
+                       'version': version,
+                       'dx_OS': build_config['dnanexus_OS']
                       }
         return environment
 
 class WorkflowConfig:
 
-    def __init__(self, path_list, project_dxid, name):
+    def __init__(self, path_list, project_dxid, name, dx_OS):
         ''' Dev: Eventually only rebuild applets/workflows if the applet source
                  has changed.
         '''
 
         self.name = name
         self.project_dxid = project_dxid
+        self.dx_OS = dx_OS
 
-        workflow_json_basename = self.name + '.json'
-        self.config_path = os.path.join(path_list.workflow_config_templates, 
-                                            workflow_json_basename
-                                            )
+        #workflow_json_basename = self.name + '.json'
+        workflow_json_template = self.name + '.json'
+        self.template_path = os.path.join(path_list.workflow_config_templates, 
+                                        workflow_json_template
+                                        )
 
         self.logger = configure_logger(name = self.name, 
-                               object_type = 'WorkflowConfig',
-                               path_list = path_list
-                               )
+                                       object_type = 'WorkflowConfig',
+                                       path_list = path_list
+                                       )
         
+        # DEV: future project will be to just update exiting development workflows
         self.new_workflow = True    # Always building new applets/workflows, now
 
         self.attributes = None
         self.object = None
         self.object_dxid = None
         self.edit_version = None
-        self.dx_OS = None           # Not in use
 
         self.stages = {}
         self.applets = {}
 
         ## Get workflow attributes - should be part of __init__ I think
         self.dx_login_check()
-        self.get_workflow_attributes()
+        self.read_workflow_template()
 
         if not self.project_dxid and not self.object_dxid:
             self.project_dxid = self.create_new_workflow_project()
@@ -196,18 +214,15 @@ class WorkflowConfig:
         project_dxid = dxpy.api.project_new(input_params={'name' : self.name})['id']
         return project_dxid
 
-    def get_workflow_attributes(self):
+    def read_workflow_template(self):
         
-        with open(self.config_path, 'r') as CONFIG:
+        with open(self.template_path, 'r') as CONFIG:
             self.attributes = json.load(CONFIG)
-            
-        self.edit_version = self.attributes['edit_version']
-        self.dx_OS = self.attributes['dx_OS']
 
         self.applets = self.attributes['applets']
         self.stages = self.attributes['stages']
 
-    def create_workflow(self, path, properties):
+    def create_workflow(self, path, properties=None, details=None):
         #if self.object_dxid:
         if self.new_workflow == False:
             self.object = dxpy.DXWorkflow(self.object_dxid)
@@ -217,7 +232,8 @@ class WorkflowConfig:
                                               name =  self.name,
                                               project = self.project_dxid,
                                               folder = path,
-                                              properties = properties
+                                              properties = properties,
+                                              details = details
                                               )
             self.object_dxid = self.object.describe()['id']
 
@@ -255,7 +271,7 @@ class WorkflowConfig:
         standard_inputs = self.stages[stage_index]['input']
         for name in standard_inputs:
             if name == 'applet_build_version':
-                version_label = self.get_version_label()
+                version_label = get_version_label()
                 self.stages[stage_index]['input']['applet_build_version'] = version_label
                 stage_input[name] = version_label
             elif name == 'applet_project':
@@ -298,45 +314,30 @@ class WorkflowConfig:
                                  stage_input = stage_input
                                 )
 
-    def update_config_file(self, path_list):
-        ''' Description: Open workflow config file, get 'date_created' value,
-                         and rename to 'external_rscs_<date_created>.json'
+    def write_workflow_json(self, path_list, version):
+        ''' Description: Create new JSON file with workflow configuration'
         '''
         
-        # Check if there is an existing configuration file and archive it
-        existing_config_path = self.config_path
-        basename = os.path.basename(existing_config_path)
-        name_elements = basename.split('.')
-        filename = name_elements[0]
-        filetype = name_elements[1]
-        if os.path.isfile(existing_config_path):
-            with open(existing_config_path, 'r') as EXIST:
-                existing_json = json.load(EXIST)
-            existing_date_created = existing_json['date_created']
-            existing_config_archived = '%s_%s.%s' % (filename, 
-                                                     existing_date_created, 
-                                                     filetype)
-            existing_config_archived_path = os.path.join(path_list.workflow_config_templates, 'archive',
-                                                         existing_config_archived)
-            os.rename(existing_config_path, existing_config_archived_path)
-        
+        # os.path.join(path_list.launchpad, workflow_name, version_stamp, workflow_name+'.json')
+        version_label = get_version_label()
+        workflow_json_dir = os.path.join(path_list.launchpad,
+                                         'workflow_jsons', 
+                                         self.name, 
+                                         version_label
+                                         )
+        _make_new_dir(workflow_json_dir)
+        workflow_json_path = os.path.join(workflow_json_dir, self.name + '.json')
+
         # Create new JSON file
         self.attributes['date_created'] = str(datetime.datetime.now()).split()[0] # yyyy-mm-dd
         self.attributes['edit_version'] = self.edit_version
+        self.attributes['dx_OS'] = self.dx_OS
+        self.attributes['workflow_dxid'] = self.object_dxid
+        self.attributes['workflow_project_dxid'] = self.project_dxid
+        self.attributes['version_label'] = version_label
 
-        with open(self.config_path, 'w') as CONFIG:
+        with open(workflow_json_path, 'w') as CONFIG:
             json.dump(self.attributes, CONFIG, sort_keys=True, indent=4)
-
-    def get_version_label(self):
-        timestamp = str(datetime.datetime.now()).split()[0] # yyyy-mm-dd
-        current_commit = self._get_git_commit().rstrip()
-        version_label = '%s_%s' % (timestamp, current_commit)
-        return version_label
-
-    def _get_git_commit(self):
-        # NOTE: not at all confident this is optimal solution
-        commit = subprocess.check_output(['git', 'describe', '--always'])
-        return commit 
 
 class AppletBuild:
 
@@ -362,9 +363,9 @@ class AppletBuild:
 
         # Create resource managers
         self.logger.info('Creating resource managers')
-        internal_rsc_manager = InternalRscsManager(path_list.internal_rscs_json, path_list)
+        internal_rsc_manager = InternalRscManager(path_list.internal_rscs_json, path_list)
         external_rscs_project_dxid = self.environment['external_rscs_dxid']
-        external_rsc_manager = ExternalRscsManager(external_rscs_project_dxid, path_list)
+        external_rsc_manager = ExternalRscManager(external_rscs_project_dxid, path_list)
 
         # Load applet resources JSON
         self.logger.info('Loading applet resources config file: %s' % path_list.applet_rscs)
@@ -458,7 +459,7 @@ class Applet:
                                            path_list = path_list
                                            )
         # DEV: Think I'm going to deprecate version_label; moving to project/folder model
-        self.version_label = self.get_version_label()
+        self.version_label = get_version_label()
         
         self.internal_rscs = []     # Filled by self.add_rsc()
         self.bundled_depends = []   # External resources
@@ -496,8 +497,8 @@ class Applet:
         self.src_path = '%s/%s/%s/src' % (path_list.launchpad, self.name, self.version_label)
         self.rscs_path = '%s/%s/%s/resources' % (path_list.launchpad, self.name, self.version_label) 
 
-        self._make_new_dir(self.src_path)
-        self._make_new_dir(self.rscs_path)
+        _make_new_dir(self.src_path)
+        _make_new_dir(self.rscs_path)
 
         # Copy source code into applet directory
         shutil.copy(self.code_path, '%s/%s' % (self.src_path, code_basename))
@@ -609,22 +610,6 @@ class Applet:
                 json.dump(config_attributes, OUT, sort_keys=True, indent=4)
         else:
             self.logger.info('No internal resources uploaded for applet %s' % self.name)
-
-    ## Private functions
-    def _make_new_dir(self, directory):
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-
-    def _get_git_commit(self):
-        # NOTE: not at all confident this is optimal solution
-        commit = subprocess.check_output(['git', 'describe', '--always'])
-        return commit 
-
-    def get_version_label(self):
-        timestamp = str(datetime.datetime.now()).split()[0] # yyyy-mm-dd
-        current_commit = self._get_git_commit().rstrip()
-        version_label = '%s_%s' % (timestamp, current_commit)
-        return version_label
 
 class ExternalRscManager:
 
@@ -806,12 +791,15 @@ class PathList:
                                                'external_resources.json'
                                                )
     
+
     def update_dnanexus_os(self, dnanexus_os):
+        ''' Used by external_rscs_json '''
         self.dnanexus_os = dnanexus_os
         self.external_rscs_json = os.path.join(self.external_rscs, 
                                                self.dnanexus_os, 
                                                'external_resources.json'
                                               )
+
     def describe(self):
         self.__dict__
 
@@ -839,6 +827,16 @@ def configure_logger(name, object_type, path_list):
 
     return logger
 
+def get_version_label():
+    timestamp = str(datetime.datetime.now()).split()[0] # yyyy-mm-dd
+    git_commit = subprocess.check_output(['git', 'describe', '--always']).rstrip()
+    version_label = '%s_%s' % (timestamp, git_commit)
+    return version_label
+
+def _make_new_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
 def main():
 
     # Parse arguments
@@ -861,8 +859,7 @@ def main():
     # Read 'builder.json' configuration file for building workflows/applets
     with open(path_list.build_json, 'r') as build_json:
         build_config = json.load(build_json)
-    dnanexus_os = build_config['dnanexus_os']
-    path_list.update_dnanexus_os(dnanexus_os)
+    dnanexus_os = build_config['dnanexus_OS']
 
     internal_rsc_manager = InternalRscManager(path_list)
     external_rsc_manager = ExternalRscManager(path_list,
