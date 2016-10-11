522d0d11a9859a55e324d7dae8e013f23104eae0
Paul Billing-Ross <pbilling@stanford.edu>
2016-10-11 15:57:59 -0700

    Retro add tags to ENCODE projects, etc

--- scripts/help/add_tags.py
+++ scripts/help/add_tags.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python
- 
+
+import re 
 import sys
 import dxpy
 
 from itertools import chain
 
-#sys.path.append('/Users/pbilling/Documents/GitHub/trajectoread/internal_resources/python_packages')
+sys.path.append('/Users/pbilling/Documents/GitHub/trajectoread/internal_resources/python_packages')
 from scgpm_lims import Connection
 from scgpm_lims import RunInfo
 
-def get_experiment(id):
+def get_experiment(experiment_id):
 
     experiment_dict = {
                    1: "Whole-Genome DNA Fragments",
@@ -32,14 +33,50 @@ def get_experiment(id):
                    18: "Single-Cell",
                    19: "qPCR"
                   }
-    return experiment_dict(id)
+    return experiment_dict[experiment_id]
 
-def get_organism(id):
+def get_organism(organism_id):
     
     organisms = {
                  1: 'Human'
                 }
-    return organisms[id]
+    return organisms[organism_id]
+
+def add_project_tags(project_name, project_dxid, conn):
+
+    print project_name
+    elements = project_name.split('_')
+    run_name = '_'.join(elements[0:4])
+    lane = elements[4]
+    match = re.search(r'L(\d)', lane)
+    lane_index = match.group(1)
+
+    # Lookup LIMS lane info
+    run_info = RunInfo(conn=conn, run=run_name)
+    lane_info = run_info.get_lane(lane_index)
+    
+    # Get experiment & organism id
+    dna_library_id = lane_info['dna_library_id']
+    dna_library_info = conn.getdnalibraryinfo(dna_library_id)
+    experiment_type_id = dna_library_info['experiment_type_id']
+    organism_id = dna_library_info['organism_id']
+
+    # Add these functions
+    experiment_type = get_experiment(experiment_type_id)
+    organism_name = get_organism(organism_id)
+
+    ## GET PROJECT NAMES
+
+    # Determine whether project is production or release
+    if len(elements) == 5:
+        tags = ['ENCODE', experiment_type, organism_name, 'Production']
+    elif len(elements) > 5:
+        tags = ['ENCODE', experiment_type, organism_name]
+    print tags
+    dxid = dxpy.api.project_add_tags(
+                                     object_id = project_dxid,
+                                     input_params = {'tags':tags})
+    return dxid
 
 def main():
     '''Add tags to ENCODE projects.
@@ -63,40 +100,19 @@ def main():
 
     project_gen = chain(sreq_gen, znf_gen)
     for project in project_gen:
-        name = project['describe']['name']
-        print name
-        elements = name.split('_')
-        run_name = '_'.join(elements[0:4])
-        lane = elements[4]
-        match = re.search(r'L(\d)', lane)
-        lane_index = match.group(1)
+        release_name = project['describe']['name']
+        release_id = project['id']
+        # Add tags to release project
+        add_project_tags(release_name, release_id, conn)
 
-        # Lookup LIMS lane info
-        run_info = RunInfo(conn=conn, run=run_name)
-        lane_info = run_info.get_lane(lane_index)
-        
-        # Get experiment & organism id
-        dna_library_id = lane_info['dna_library_id']
-        dna_library_info = conn.getdnalibraryinfo(dna_library_id)
-        experiment_type_id = dna_library_info['experiment_type_id']
-        organism_id = dna_library_info['organism_id']
-
-        # Add these functions
-        experiment_type = get_experiment(experiment_type_id)
-        organism_name = get_organism(organism_id)
-
-        ## GET PROJECT NAMES
-
-        # Determine whether project is production or release
-        if len(elements) == 5:
-            tags = ['ENCODE', experiment_type, organism_name, 'Production']
-        elif len(elements) == 6:
-            tags = ['ENCODE', experiment_type, organism_name, project_name]
-        '''
-        dxpy.api.project_add_tags(
-                                  object_id = project['id'],
-                                  input_params = {tags: tags})
-        '''
+        # Add tags to production project
+        elements = release_name.split('_')
+        production_name = '_'.join(elements[0:5])
+        production = dxpy.find_one_project(
+                                           name = production_name,
+                                           more_ok = False,
+                                           describe = True)
+        add_project_tags(production_name, production['id'], conn)
 
 if __name__ == "__main__":
     main()
